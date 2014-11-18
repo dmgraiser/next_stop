bf444ffb34f314f1a49ae0c1b7ccd4b45841f933
Doug Graiser <dmgraiser@gmail.com>
2014-11-17 20:32:22 -0500

    fix line spacing and update comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,16 +3,17 @@ module ApplicationHelper
 		def hash_and_msg_generator(results, busnum)
 			#we will have a couple messages to tell the user whether or not the bus number is valid.
 			@message = ""
-			#we want to provide the user with the name of the next stop for their bus. The best way to save two related items is a hash!
-			# @nextstop = {}
+			#we want to provide the user with the name of the next stop for their bus by calling the TIMEPOINT where the VEHICLE number matches the user's input.
 			@results.each do |result|
 				if result["VEHICLE"] == busnum.to_s
-					#if the User's bus number matching an active bus in the API data, we will save that info in our hash
+					#if the User's bus number matching an active bus in the API data, we will rename @nextstop
 					@nextstop = result["TIMEPOINT"]
-					#the Route is the key and the next stop is the value
+					
 					@message = "Your next stop will be:"
+					#break needed to keep the loop from continuing throught the rest of the data
 					break
 				else
+					#user's input doesn't match any active busses
 					@message = "I'm sorry, but that isn't an active bus. Please try again."
 				end
 			end
